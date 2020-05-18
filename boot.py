64b9d4a6227f73cb927509d7c569ec69204a1280
watrt <xb100@qq.com>
2020-05-18 12:24:04 +0800

    更新中文支持

--- boot.py
+++ boot.py
@@ -1,1 +1,1 @@
-import os# This file is executed on every boot (including wake-boot from deepsleep)import espesp.osdebug(None)#import webrepl#webrepl.start()
+import os# This file is executed on every boot (including wake-boot from deepsleep)import espesp.osdebug(None)#import webrepl#webrepl.start()
